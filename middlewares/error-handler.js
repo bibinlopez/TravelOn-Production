6dc60b8863c4622f1fa17c059268b96e132b2347
John <bibinlopez36@gmail.com>
2023-06-03 14:03:35 +0530

    manual error msg changed

--- middlewares/error-handler.js
+++ middlewares/error-handler.js
@@ -1,10 +1,11 @@
 const { CustomAPIError } = require('../errors/custom-error')
 
-const errorHandlerMiddlerware = (err, req, res ,next) => {
-   if (err instanceof CustomAPIError ) {
-      return res.status(err.statusCode).json({ msg: err.message})
+const errorHandlerMiddlerware = (err, req, res, next) => {
+   if (err instanceof CustomAPIError) {
+      return res.status(err.statusCode).json({ msg: err.message })
    }
-   return res.status(500).json({ msg: 'Something went wrong try again later' })
+   // return res.status(500).json({ msg: 'Something went wrong try again later' })
+   return res.status(500).json({ msg: err.message })
 }
 
 module.exports = errorHandlerMiddlerware 
