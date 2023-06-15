ee4df3ed1810d411aa443a80efdf31f2b5c89e01
Bibin Lopez <bibinlopez36@gmail.com>
2023-06-15 14:59:45 +0530

    bug

--- routes/place.js
+++ routes/place.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const path = require('path')
 const multer = require('multer')
-const multiparty = require ('../middlewares/multiparty')
 
 const { addFromExcel,
    getAllPlaces,
